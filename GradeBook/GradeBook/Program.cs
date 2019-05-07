5e3140b2cc93fd9d26fbf8635220e7ba19c18f1e
Kayla Mylius <Kayla Mylius@DESKTOP-VF3JNFI>
2019-05-07 15:24:05 -0700

    Using a list of grades and finding and formatting the average.

--- GradeBook/GradeBook/Program.cs
+++ GradeBook/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -7,25 +8,18 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var numbers = new[] { 12.7, 10.3, 6.11 };
+            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
 
             var result = 0.0;
 
-            foreach(double number in numbers)
+            foreach(double number in grades)
             {
                 result += number;
             }
+            result /= grades.Count;
+            Console.WriteLine($" The average grade is { result:N2}");
 
-            Console.WriteLine(result);
-
-            if (args.Length > 0)
-            {
-                Console.WriteLine($"Hello {args[0]} !");
-            }
-            else
-            {
-                Console.WriteLine("Hello World!");
-                Console.WriteLine("I AM GROOT!");
-            }
         }
     }
 }
