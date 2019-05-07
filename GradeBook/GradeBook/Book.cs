843fd60f9a05d43121d64036a1d938ce0d32edd6
Kayla Mylius <Kayla Mylius@DESKTOP-VF3JNFI>
2019-05-07 16:33:38 -0700

    Created a new method ShowStats to clean up the my code in my main GradeBook project.

--- GradeBook/GradeBook/Book.cs
+++ GradeBook/GradeBook/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -12,6 +13,25 @@ namespace GradeBook
         public void AddGrade(double grade)
         {
             grades.Add(grade);
+        }
+
+        public void ShowStats()
+        {
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowGrade = double.MaxValue;
+
+            foreach (double number in grades)
+            {
+                highGrade = Math.Max(number, highGrade);
+                lowGrade = Math.Min(number, lowGrade);
+                result += number;
+            }
+            result /= grades.Count;
+            Console.WriteLine($" The average grade is { result:N2}");
+            Console.WriteLine($" The highest grade is { highGrade:N2}");
+            Console.WriteLine($" The lowest grade is { lowGrade:N2}");
+
         }
 
         List<double> grades;
