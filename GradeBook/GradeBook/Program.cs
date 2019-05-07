e6ead0e2556b04d280eaac9d62b09d072e819f14
Kayla Mylius <Kayla Mylius@DESKTOP-VF3JNFI>
2019-05-07 14:41:17 -0700

    Creating two variables and adding them together.

--- GradeBook/GradeBook/Program.cs
+++ GradeBook/GradeBook/Program.cs
@@ -6,6 +6,11 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            var x = 34.1;
+            var y = 45.2;
+            var result = x + y;
+            Console.WriteLine(result);
+
             if (args.Length > 0)
             {
                 Console.WriteLine($"Hello {args[0]} !");
