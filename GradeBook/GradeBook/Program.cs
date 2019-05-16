375445ee2854456daeace81aa2b84c02395be6cd
Kayla Mylius <Kayla Mylius@DESKTOP-VF3JNFI>
2019-05-15 17:18:14 -0700

    switch / case for my letter grades

--- GradeBook/GradeBook/Program.cs
+++ GradeBook/GradeBook/Program.cs
@@ -16,7 +16,7 @@ namespace GradeBook
             Console.WriteLine($" The average grade is { stats.Average:N2}");
             Console.WriteLine($" The highest grade is { stats.High:N2}");
             Console.WriteLine($" The lowest grade is { stats.Low:N2}");
-
+            Console.WriteLine($"The letter grade is {stats.Letter}");
         }
     }
 }
